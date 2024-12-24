ebe2c61d61e0c070230f9d2228e6d36ed7ebf11b
Danusha <danuv3087@gmail.com>
2024-12-24 12:12:02 +0530

    2nd Commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-        MyData myrequirement = new MyData();
-        myrequirement.setMyName("Danusha");
-        myrequirement.setManufacturerYear(0);
-        myrequirement.setMyValue(0);
+        MyData myRequirement = new MyData();
+        myRequirement.setMyName("Danusha Pramode");
+        myRequirement.setManufacturerYear(0);
+        myRequirement.setMyValue(0);
 
 
         CheckMyData checkmydata = new CheckMyData();
-        if (myrequirement.getPrice() > 0 && myrequirement.getManufacturerYear() > 0) {
-            checkmydata.findCars(myrequirement);
+        if (myRequirement.getPrice() > 0 && myRequirement.getManufacturerYear() > 0) {
+            checkmydata.findCars(myRequirement);
         } else {
             checkmydata.findCars();
         }
